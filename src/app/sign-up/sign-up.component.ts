98fd391a8a679dbfd9f66a2dd41d374c037a65e1
AntasSingh <antas9991@gmail.com>
2024-08-09 20:13:11 +0100

    fixed signup error

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -14,6 +14,9 @@ export class SignUpComponent {
   reEnterPassword: string = '';
   passwordError: string = '';
   emailError: string = ''; // New property for email validation
+  loading: boolean =true;
+  errorMesage: string = '';
+  errorStatus: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -24,6 +27,7 @@ export class SignUpComponent {
   }
 
   onSubmit(): void {
+    this.loading =true;
     // Check if the passwords match
     if (this.password !== this.reEnterPassword) {
       this.passwordError = 'Passwords do not match';
@@ -48,14 +52,18 @@ export class SignUpComponent {
     };
 
     this.authService.register(registrationData).subscribe(
+      
       responseMessage => {
         console.log('Registration successful:', responseMessage);
         this.router.navigate(['/login']);
         // Handle success (e.g., navigate to login page or show a success message)
+        this.loading = false;
       },
       error => {
         console.error('Registration failed:', error);
         // Handle error (e.g., show an error message)
+        this.loading =true;
+        this.emailError = error.message;
       }
     );
   }
